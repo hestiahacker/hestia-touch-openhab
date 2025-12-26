0a42b9533840b0e40c88213c82009052162e0875
hestia <hestia@hax0rbana.org>
2025-12-26 15:24:34 -0600

    fix: getwifiinfo.sh to work on all versions of Debian

--- home/pi/scripts/getwifiinfo.sh
+++ home/pi/scripts/getwifiinfo.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-
 # Returns Wifi signal strength in percentage
-
-RSSI=$(/sbin/iwconfig wlan0 | grep 'Signal level' | awk '{print $4}' | cut -d= -f2 | cut -d/ -f1);
+RSSI=$(/sbin/iwconfig wlan0 | grep 'Signal level' | awk '{print $4}' | sed 's/.*[=:]//g' | cut -d/ -f1);
 #RSSI=-99
 #echo $RSSI
 
-if [ -z "$RSSI" ]
-then
+if [ -z "$RSSI" ]; then
   echo "-"
-elif [[ $RSSI -le -100 ]]
-then
+elif [ $RSSI -le -100 -o $RSSI -eq 0 ]; then
   echo "0"
-elif [[ $RSSI -ge -50 ]]
-then
+elif [ $RSSI -ge -50 ]; then
   echo "100"
 else
   echo $[2 * (RSSI + 100)] | bc
 fi
-
